5bf614eb0dabf30793ced7eaae7da9a377980862
3FE 3LE (Frank Licona) <3l33f3@gmail.com>
2020-09-18 15:49:17 -0500

    try #7

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,38 @@
-import React, { Component } from "react";
-import Router from "next/router";
 
-export default class Index extends Component {
-  componentDidMount = () => {
-    Router.push("/login");
-  };
+import { Grow, Slide } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import React, { useState } from 'react'
+// import components
+import LandingLayout from '../components/layouts/LandingLayout'
+import { Loader } from '../components/Loader'
 
-  render() {
-    return <div />;
-  }
+export default function Index() {
+
+
+
+  const router = useRouter();
+
+  const [show, setShow] = useState(true)
+
+  setTimeout(() => {
+    setShow(false)
+  }, 1000);
+
+  setTimeout(() => {
+    setShow(true)
+    router.push('/login')
+  }, 3000);
+
+  return (
+    <>
+      <LandingLayout>
+        <Loader open={show} />
+        <Slide direction="down" in={!show} timeout={{ enter: 1500, exit: 1000 }}>
+          <div className="flex items-center justify-center text-gray-100 font-black text-6xl uppercase">
+            <h1>Bienvenido a la subasta agraria</h1>
+          </div>
+        </Slide>
+      </LandingLayout>
+    </>
+  )
 }
